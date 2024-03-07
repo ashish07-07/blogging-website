bf19f117f32ccd3197934b49a70713328c097d86
B <bkashishh07@gmail.com>
2024-03-07 23:37:46 +0530

    mild changes
    gave different routes to the folders

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,33 +3,14 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from "hono/jwt";
 
-import { userRouter } from "./routes/user";
-
-const app = new Hono<{
+export const userRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
 }>();
 
-app.route("/api/v1/user", userRouter);
-app.route("/api/v1/blog"), blogRouter;
-app.use("/api/v1/blog/*", async function (c, next) {
-  const header = c.req.header("authorization") || "";
-
-  const token = header.split(" ")[1];
-  const res = await verify(token, c.env.JWT_SECRET);
-
-  if (res.id) {
-    next();
-  } else {
-    c.status(403);
-    return c.json({
-      msg: "invalid crededntials ",
-    });
-  }
-});
-app.post("/api/v1/user/signup", async function (c) {
+userRouter.post("/signup", async function (c) {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
@@ -75,17 +56,3 @@ app.post("/api/v1/user/signin", async function (c) {
     token,
   });
 });
-
-// app.post("/api/v1blog", function () {});
-
-// app.put("/api/v1/blog", function (c) {});
-
-app.get("/api/v1/blog/:id", function (c) {
-  const body = c.body;
-
-  return c.json({
-    msg: "hi bro  wts up ",
-  });
-});
-
-export default app;
