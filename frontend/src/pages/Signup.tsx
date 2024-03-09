9a8b1828e7ba7a77a11181f7e381aa977b2bae1a
B <bkashishh07@gmail.com>
2024-03-09 21:49:42 +0530

    quote component
    quote component added

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,3 +1,11 @@
+import { Quote } from "../components/Quote";
 export function Signup() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="grid grid-cols-2">
+        <div>hi</div>
+        <Quote></Quote>
+      </div>
+    </div>
+  );
 }
