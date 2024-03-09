c3b7d9ab3b573cbe7c75b1882ff473920542b532
B <bkashishh07@gmail.com>
2024-03-09 22:44:50 +0530

    styling
    added css

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -2,17 +2,20 @@ export function Quote() {
   return (
     <div className="bg-slate-200 h-screen flex justify-center flex-col">
       <div className="flex justify-center">
-        <div className=" max-w-lg text-center  text-3xl font-bold">
-          "The customer support I recieved ws exceptional. THe support team went
-          above and below to adress my concerns"
-        </div>
-      </div>
-      <div className=" max-w-md text-center  text-xl font-semibold">
-        Julies Winfield
-      </div>
+        <div className=" max-w-lg ">
+          <div className="text-3xl font-bold">
+            "The customer support I recieved ws exceptional. THe support team
+            went above and below to adress my concerns"
+          </div>
 
-      <div className=" max-w-md text-center  text-sm font-semibold text-slate-400">
-        CEO | Acme corp
+          <div className=" max-w-md text-center  text-xl font-semibold">
+            Julies Winfield
+          </div>
+
+          <div className=" max-w-md text-center  text-sm font-semibold text-slate-400">
+            CEO | Acme corp
+          </div>
+        </div>
       </div>
     </div>
   );
