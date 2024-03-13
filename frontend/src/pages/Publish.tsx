1d72204b9839b96bf7bf6e65e209d4b41ca2f179
B <bkashishh07@gmail.com>
2024-03-14 00:44:19 +0530

    final changes

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -23,16 +23,14 @@ export function Publish() {
       }
     );
     console.log(res);
-    navigate(`/blog/${res.data.msg.id}`);
+
+    navigate("/blogs");
   }
   return (
     <div>
       <Appbar></Appbar>
       <div className="flex justify-center w-full">
         <div className="max-w-screen-lg w-full">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            emial
-          </label>
           <input
             type="text"
             className="w-full bg-grey-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
