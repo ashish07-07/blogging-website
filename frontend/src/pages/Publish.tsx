1cafe4ca2c4a888b8ca9f4fa62db9f9858d30e26
B <bkashishh07@gmail.com>
2024-03-13 14:29:13 +0530

    Update Publish.tsx

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,14 +1,21 @@
+import { Appbar } from "../components/Appbar";
+
 export function Publish() {
   return (
     <div>
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-        emial
-      </label>
-      <input
-        type="text"
-        className="bg-grey-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-        placeholder="Title"
-      ></input>
+      <Appbar></Appbar>
+      <div className="flex justify-center w-full">
+        <div className="max-w-screen-lg w-full">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            emial
+          </label>
+          <input
+            type="text"
+            className=" w-full bg-grey-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            placeholder="Title"
+          ></input>
+        </div>
+      </div>
     </div>
   );
 }
